138acbd932da4d7998e6bb12320088d671fd3958
osinsomkuwar-27 <osinsomkuwar26@gmail.com>
2025-07-21 18:59:05 +0530

    A1

---
+++ Arrays/Linearsearch.java
@@ -0,0 +1,30 @@
+
+public class Linearsearch {
+    public static int linearsearch(int numbers[], int key) {
+        
+        // Loop to iterate through the entire array one by one
+        for (int i = 0; i < numbers.length; i++) {
+            // If the ith element matches the key we return its index i.e i
+            if (numbers[i] == key) {
+                return i;
+            }
+        }
+        // If the key is not found we return -1 (also as we have declared the return type of our function int thats why we are returning -1)
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        int numbers[] = {2, 4, 6, 8, 10, 12, 14, 16};
+        int key = 10;
+        
+        // Calling the linearsearch method and storing the result (index)
+        int index = linearsearch(numbers, key);
+
+        // Checking if the element was found or not
+        if (index == -1) {
+            System.out.println("Element is not present in array");
+        } else {
+            System.out.println("Key is at index : " + index);
+        }
+    }
+}
