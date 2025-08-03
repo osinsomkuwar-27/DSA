4ca3b4914cd65c92874c838899ab917f680ab283
osinsomkuwar-27 <osinsomkuwar26@gmail.com>
2025-08-03 22:32:49 +0530

    bubblesort

--- Sorting/Bubblesort.java
+++ Sorting/Bubblesort.java
@@ -1,27 +1,29 @@
 public class Bubblesort {
     public static void bubbleSort(int[] arr) {
-        int n = arr.length;
-        boolean swapped;
-        for (int i = 0; i < n - 1; i++) {
-            swapped = false;
-            for (int j = 0; j < n - i - 1; j++) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    // swap arr[j] and arr[j+1]
+                    // swap 
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
-                    swapped = true;
                 }
             }
         }
     }
 
+    public static void printArray(int[] arr) {
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         int[] arr = {64, 34, 25, 12, 22, 11, 90};
         bubbleSort(arr);
-        System.out.println("Sorted array: ");
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }
+        System.out.println("Sorted array:");
+        printArray(arr);
+        
     }
 }
