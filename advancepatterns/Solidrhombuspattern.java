1bff1b7933b01ca683c0b71e27b7bb39e4906d9d
osinsomkuwar-27 <osinsomkuwar26@gmail.com>
2025-12-20 11:26:31 +0530

    Advanced patterns using functions

--- advancepatterns/Solidrhombuspattern.java
+++ advancepatterns/Solidrhombuspattern.java
@@ -1,6 +1,16 @@
-// public class Solidrhombuspattern{
-//     public 
-//     public static void main(String[] args) {
-        
-//     }
-// }
+public class Solidrhombuspattern{
+    public static void solidrhombus(int n){
+        for(int i = 1;i<=n;i++){
+            for(int j=1;j<=(n-i);j++){
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        solidrhombus(5);
+    }
+}
